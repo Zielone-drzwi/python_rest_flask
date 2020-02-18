a9c5db7059d452c92d7cbb6d1bcd7c9abda4654f
Arrato <jacek.niewiadomski@gmail.com>
2020-02-18 22:04:32 +0100

    zmiana na fejkowa baze

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 
-db_connect = create_engine('sqlite:///chinook.db')
+db_connect = create_engine('sqlite:///fejkowa.db')
 app = Flask(__name__)
 api = Api(app)
 
@@ -12,49 +12,41 @@ api = Api(app)
 class Employees(Resource):
     def get(self):
         conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+        query = conn.execute("select * from fejkowa") # This line performs query and returns json result
+        return {'id': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
     
     def post(self):
         conn = db_connect.connect()
         print(request.json)
-        LastName = request.json['LastName']
-        FirstName = request.json['FirstName']
-        Title = request.json['Title']
-        ReportsTo = request.json['ReportsTo']
-        BirthDate = request.json['BirthDate']
-        HireDate = request.json['HireDate']
-        Address = request.json['Address']
-        City = request.json['City']
-        State = request.json['State']
-        Country = request.json['Country']
-        PostalCode = request.json['PostalCode']
-        Phone = request.json['Phone']
-        Fax = request.json['Fax']
-        Email = request.json['Email']
+        data = request.json['data']
+        lat = request.json['lat']
+        lon = request.json['lon']
+        kompas = request.json['kompas']
+        napiecie = request.json['napiecie']
+        pochylenie = request.json['pochylenie']
+        przechylenie = request.json['przechylenie']
 
-        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-                             '{13}')".format(LastName,FirstName,Title,
-                             ReportsTo, BirthDate, HireDate, Address,
-                             City, State, Country, PostalCode, Phone, Fax,
-                             Email))
-        return {'status':'success'}
+    
+
+        query = conn.execute("insert into fejkowa values(null,'{1}','{2}','{3}', \
+                             '{4}','{5}','{6}',{7})".format(data,lat,lon,
+                             kompas, napiecie, pochylenie, przechylenie))
+        return {'status':'ok'}
 
     
 class Tracks(Resource):
     def get(self):
         conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute("select data, lat, lon, kompas from fejkowa;")
+        result = {'dane': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
     
 class Employees_Name(Resource):
     def get(self, employee_id):
         conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute("select * from fejkowa where id =%d "  %int(employee_id))
+        result = {'dane': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
 
